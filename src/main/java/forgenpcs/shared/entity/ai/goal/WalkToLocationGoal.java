72f33031f375ffc7acc6b5ce9204cb7da3376914
Pieter12345 <P.J.S.Kools@student.tudelft.nl>
2021-07-18 18:08:33 +0200

    Enforce exact NPC target location + Add path finish callback

--- src/main/java/forgenpcs/shared/entity/ai/goal/WalkToLocationGoal.java
+++ src/main/java/forgenpcs/shared/entity/ai/goal/WalkToLocationGoal.java
@@ -11,11 +11,18 @@ public class WalkToLocationGoal extends Goal {
 	private final MobEntity entity;
 	private final double speed;
 	private final Vector3d targetLocation;
+	private final WalkToLocationCallback callback;
 	
 	public WalkToLocationGoal(MobEntity entity, Vector3d targetLocation, double speed) {
+		this(entity, targetLocation, speed, null);
+	}
+	
+	public WalkToLocationGoal(
+			MobEntity entity, Vector3d targetLocation, double speed, WalkToLocationCallback callback) {
 		this.entity = entity;
 		this.targetLocation = targetLocation;
 		this.speed = speed;
+		this.callback = callback;
 		this.setMutexFlags(EnumSet.of(Goal.Flag.MOVE, Goal.Flag.JUMP));
 	}
 	
@@ -29,25 +36,39 @@ public class WalkToLocationGoal extends Goal {
 	
 	@Override
 	public boolean shouldExecute() {
-		return this.targetLocation != null && (this.entity.getPosX() != this.targetLocation.x
-				|| this.entity.getPosY() != this.targetLocation.y || this.entity.getPosZ() != this.targetLocation.z);
+		return this.targetLocation != null && this.entity.getDistanceSq(this.targetLocation) >= 0.01d;
 	}
 	
 	@Override
 	public void startExecuting() {
 		PathNavigator navigator = this.entity.getNavigator();
 		navigator.setSearchDepthMultiplier(1000f);
-		navigator.setPath(navigator.pathfind(
+		navigator.setPath(this.entity.getNavigator().pathfind(
 				this.targetLocation.x, this.targetLocation.y, this.targetLocation.z, 0), this.speed);
 	}
 	
 	@Override
 	public boolean shouldContinueExecuting() {
-		return !this.entity.getNavigator().noPath();
+		boolean shouldContinueExecuting = !this.entity.getNavigator().noPath();
+		if(!shouldContinueExecuting) {
+			this.entity.setPosition(this.targetLocation.x, this.targetLocation.y, this.targetLocation.z);
+			if(this.callback != null) {
+				try {
+					this.callback.onPathFinished();
+				} catch (Throwable t) {
+					t.printStackTrace();
+				}
+			}
+		}
+		return shouldContinueExecuting;
 	}
 	
 	@Override
 	public void resetTask() {
 		this.entity.getNavigator().clearPath();
+	}
+	
+	public static interface WalkToLocationCallback {
+		void onPathFinished();
 	}
 }
